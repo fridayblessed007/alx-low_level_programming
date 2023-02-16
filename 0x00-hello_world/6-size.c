4d5c9e3fd9cb89b03611e903a6705fab8ee3884a
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-16 07:19:39 -0800

    Size of computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,16 @@
  */
 
 int main(void)
-{
-	printf("size of a char: %ld byte(s)\n", sizeof(char));
-	printf("size of an int: %ld byte(s)\n", sizeof(int));
-	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("size of a float: %ld byte(s)\n", sizeof(float));
+{   
+	              printf("size of a char: %lu bytes\n", sizeof(char));
+
+	                    printf("size of an int: %lu bytes\n", sizeof(int)); 
+
+	                      printf("size of a long int: %lu bytes\n", sizeof(long int));
+
+	                       printf("size of a long long int: %lu bytes\n", sizeof(long long int));
+
+	                       printf("size of a float: %lu bytes\n", sizeof(float));
 
 	return (0);
 
