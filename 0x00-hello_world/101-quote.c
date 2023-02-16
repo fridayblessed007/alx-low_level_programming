17f5eed84196c1a4d346ca69b88af862a077d424
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-16 05:07:23 -0800

    hveqwhb

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main (void)
+int main(void)
 {
-          write(STDIO_FILENO,"and that piece of art is useful
-			  
-			  -Dora Korpar 2015-10-19\n");
+          write(STDERR__FILENO,"and that piece of art is useful\"  -Dora Korpar, 2015-10-19\n"
+			  ;
 	   return(1);
 }
