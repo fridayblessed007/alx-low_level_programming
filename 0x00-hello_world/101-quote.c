54e2490f9cc558e779fb2e08371251c9156bde63
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-16 06:31:22 -0800

    6-size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-          write(STDOUT_FILE,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+          write(STDOUT_FILE,"and that piece of art is useful\" - Dora Korpar ,  2015-10-19\n",);
 			  
 	   return (1);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - C program hat prints the size of various types on the computer it is compiled and run on.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("char size: %lu bytes\n", sizeof(char));
+	printf("int size: %lu bytes\n", sizeof(int));
+	printf("short size: %lu bytes\n", sizeof(short));
+	printf("long size: %lu bytes\n", sizeof(long));
+	printf("float size: %lu bytes\n" sizeof(float));
+	printf("double size: %lu bytes\n" sizeof(double));
+	printf("long double size: %lu bytes\n" sizeof(long double));
+
+	return (0);
+
+	)
