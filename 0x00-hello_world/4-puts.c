f0bc2637e00bab5b61b7b2c39e63cf96e90e3295
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-16 03:58:33 -0800

    hello,put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #!/bin/bash
-#include <stdio.h>{
-puts("Programming is like building a multilingual puzzle");
-return 0;
+#include <stdio.h>
+int main(void){
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
