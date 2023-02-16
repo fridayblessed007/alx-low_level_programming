462b76a6b5545c916ab113cd4815d2ae1a71a63f
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-16 08:15:38 -0800

    unix..

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-          write(STDOUT_FILE,"and that piece of art is useful\" - Dora Korpar ,  2015-10-19\n",);
+          write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar,  2015-10-19\n", 59);
 			  
 	   return (1);
 }
