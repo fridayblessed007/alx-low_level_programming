b29f0b709e628c20445ce2d94a918fbd25e5e9eb
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-16 04:07:53 -0800

    please work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-int main(void){
 
+/**
+ * main- Entry point
+ *
+ * Return always 0 (Success)
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
