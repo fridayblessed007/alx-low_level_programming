4d4314944c356ff03a78bde15f6d686b939a300c
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-17 15:42:40 -0800

    print

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	{
+		int num1, num2, num3;
+		for(num1=
