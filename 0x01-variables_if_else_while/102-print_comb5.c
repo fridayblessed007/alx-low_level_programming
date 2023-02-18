562eaf0045dd9ac544a4272266dbde5b237bb8e3
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-17 21:50:32 -0800

    3ADVANCED 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 
 /**
  * main - entry point
@@ -12,28 +11,29 @@ int main(void)
 {
 	int j, i;
 
-	for (j = 0; j <= 100; j++)
+	for (j = 0; j <= 99; j++)
 	{
 		for (i = j; i <= 99; i++)
 		{
-			if(i != j)
+			if (i != j)
 			{
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				putchar((' ');
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				if (j * 100 + i ! = 9899)
 
-				{
-				putchar(',')
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
 				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+
+				if (j * 100 + i != 9899)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
-			}
-			}
+
+		}
+
+	}
 	putchar('\n');
-
-
 	return (0);
 }	
