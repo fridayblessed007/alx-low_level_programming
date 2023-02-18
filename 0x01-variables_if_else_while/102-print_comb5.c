518aa040f17de859f97ee70601087c7ffc64c310
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-17 20:57:38 -0800

    PRInt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,21 @@
 
 int main(void)
 {
-	int i, j;
+	int j, i;
 
-	for (i=0; j < 100; i++)
+	for (j = 0; j <= 100; j++)
 	{
-		for (j-0; j < 100; j++)
+		for (i = j; i <= 99; i++)
 		{
-			if(i<j)
+			if(i != j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar((' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i !=98 || j ! = 99)
+				putchar((' ');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				if (j * 100 + i ! = 9899)
+
 				{
 				putchar(',')
 				putchar(' ');
