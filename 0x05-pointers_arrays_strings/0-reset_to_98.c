2de7a6f4c094c70060fca9031b1e6a177f870145
BlessedFriday <Fridayblessed333@gmail.com>
2023-02-28 05:08:15 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *reset_to_98 - updates a integer value to 98
+ * @n: A pointer to an int that will be updated
+ * Retuen : that means is correct
+ */
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
