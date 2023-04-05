724736019487b312e0f207599b1588d96e78b153
Blessedfriday007 <fridayblessed333@gmail.com>
2023-04-04 18:36:43 -0700

    FREE

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,46 +1,39 @@
-#include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint_safe - A function that frees a list
- * @h: A pointer listint_t structure
- * Return: The size of the list that was free'd
+ * free_listint_safe - Entry point
+ * Description: Frees a listint_t list.
+ * @h: Pointer to pointer of first node in the linked list
+ *
+ * Return: The size of the list that was free’d
  */
+
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *current = *h, *node;
 	size_t counter = 0;
-	listint_t *temp;
 
-	temp = *h;
-	while (temp)
+	/* Checks if heead node is NULL */
+	if (!h || !*h)
 	{
-		temp = *h;
-		temp = temp->next;
-		free_list(temp);
+		return (counter);
+	}
+	/* Traverse through the list */
+	while (current)
+	{
+		/* Move current node to placeholder node */
+		node = current;
+		/* Move to the next node */
+		current = current->next;
+		free(node);
 		counter++;
+		/* if next pointer points back to a previous node, break the loop */
+		if (node <= current)
+		{
+			*h = NULL;
+			break;
+		}
 	}
 	*h = NULL;
-
 	return (counter);
 }
-
-/**
- * free_list - A function that frees a listint_t recursively
- * @head: A pointer to the listint_t structure
- * Return: Nothing
- */
-void free_list(listint_t *head)
-{
-	listint_t *temp;
-
-	if (head)
-	{
-		temp = head;
-		temp = temp->next;
-		free(temp);
-		free_list(temp);
-	}
-	free(head);
-}
-
